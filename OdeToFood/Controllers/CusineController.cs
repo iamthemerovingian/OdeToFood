8a0bf2b070b292163f30c17c5c973d23c82194ff
Milinda <Milinda@MILLSURFACEPRO3>
2017-04-17 21:57:49 +0700

    Played around with the attributes to a model and its actions.
    Defined our own customattributes called the LogAttributes class.
    
    Explored how to show errors on to the end user as well as show errors to the developer who is using local host.

--- OdeToFood/Controllers/CusineController.cs
+++ OdeToFood/Controllers/CusineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,20 +7,39 @@ using System.Web.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    //[Authorize]
+    [Log]
     public class CusineController : Controller
     {
         // GET: Cusine
-        public ActionResult Search(string name)
+        //[Authorize]
+        public ActionResult Search(string name = "French")
         {
             var message = Server.HtmlEncode(name);
 
-            //return Content($"Hello, you have reached the Cusine/{name} page");
+            //throw new Exception("Something Terrible Has happened!!!");
+
+            return Content($"Hello, you have reached the Cusine/{name} page");
 
             //return RedirectToAction("Index", "Home", new { name = name });
 
             //return File(Server.MapPath("~/Content/site.css"), "text/css");
 
-            return Json(new { Message = message, name = "Milinda" }, JsonRequestBehavior.AllowGet);
+            //return Json(new { Message = message, name = "Milinda" }, JsonRequestBehavior.AllowGet);
         }
+
+        //[HttpGet]
+        //public ActionResult Search()
+        //{
+        //    //var message = Server.HtmlEncode(name);
+
+        //    return Content($"Search!!");
+
+        //    //return RedirectToAction("Index", "Home", new { name = name });
+
+        //    //return File(Server.MapPath("~/Content/site.css"), "text/css");
+
+        //    //return Json(new { Message = message, name = "Milinda" }, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
