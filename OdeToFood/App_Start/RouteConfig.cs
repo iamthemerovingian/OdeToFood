83da7c09b72714b94673684d1e1be68e9949f735
Milinda <Milinda@MILLSURFACEPRO3>
2017-04-17 21:22:51 +0700

    Played around with different redirects, as well as routing to learn how to modify the routing config,cs file.
    Also now I can return a file on the server to the user. Cool huh? :D

--- OdeToFood/App_Start/RouteConfig.cs
+++ OdeToFood/App_Start/RouteConfig.cs
@@ -13,6 +13,10 @@ namespace OdeToFood
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Cuisine",
+                "Cuisine/{name}",
+                new { controller = "Cusine", action = "Search", name = UrlParameter.Optional } );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ OdeToFood/Controllers/CusineController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CusineController : Controller
+    {
+        // GET: Cusine
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+
+            //return Content($"Hello, you have reached the Cusine/{name} page");
+
+            //return RedirectToAction("Index", "Home", new { name = name });
+
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+
+            return Json(new { Message = message, name = "Milinda" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
