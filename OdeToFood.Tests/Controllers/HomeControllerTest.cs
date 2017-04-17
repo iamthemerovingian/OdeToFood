914bec2c26e74580da74b01449b7f34af054765c
Milinda <Milinda@MILLSURFACEPRO3>
2017-04-17 20:15:49 +0700

    Exlpored the basics on how MVC works.

--- OdeToFood.Tests/Controllers/HomeControllerTest.cs
+++ OdeToFood.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,8 @@ namespace OdeToFood.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.IsNotNull(result.Model);
         }
 
         [TestMethod]
