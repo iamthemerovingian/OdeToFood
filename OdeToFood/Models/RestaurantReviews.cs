71ccf97c4a6fb0e6ab393119d56f3e5ff450295d
Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-04-18 10:27:40 +0700

    Learned 2 methods of Validating the user input
    1. Use attributes built in.
    2. Use the IValidatableObject to create your own validation.
    3. Create your own Attributes and use them.

--- OdeToFood/Models/RestaurantReviews.cs
+++ OdeToFood/Models/RestaurantReviews.cs
@@ -6,8 +6,32 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
-    public class RestaurantReview
+    public class MaxWordsAttribute : ValidationAttribute
     {
+        public MaxWordsAttribute(int maxWords) : base("{0} has too many words.")
+        {
+            _maxWords = maxWords;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Split(' ').Length > _maxWords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        private readonly int _maxWords;
+    }
+    public class RestaurantReview :  IValidatableObject
+    {
+       
         public int Id { get; set; }
         [Range(1,10)]
         public int Rating { get; set; }
@@ -17,7 +41,16 @@ namespace OdeToFood.Models
 
         [Display(Name ="User Name")]
         [DisplayFormat(NullDisplayText="anonymous")]
+        [MaxWords(maxWords: 1)]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2 && ReviewerName.ToLower().StartsWith("milinda"))
+            {
+                yield return new ValidationResult($"Sorry {ReviewerName}, you can't do this!");
+            }
+        }
     }
 }
